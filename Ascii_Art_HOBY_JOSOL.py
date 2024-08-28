681bfa8cb46c025d9071537fbdc52bea3ef25a9e
MilkFish1010 <hoby.josol1010@gmail.com>
2024-08-28 12:47:28 +0800

    Add files via upload

---
+++ Ascii_Art_HOBY_JOSOL.py
@@ -0,0 +1,14 @@
+# The backlashes in the ascii art creates a syntax warning that's why I made a raw string.
+
+first_name = r"""
+  _    _       _                                     _           _           
+ | |  | |     | |               /\                  | |         (_)          
+ | |__| | ___ | |__  _   _     /  \   ___ ___       | | ___ _ __ _  ___ ___  
+ |  __  |/ _ \| '_ \| | | |   / /\ \ / __/ _ \  _   | |/ _ \ '__| |/ __/ _ \ 
+ | |  | | (_) | |_) | |_| |  / ____ \ (_|  __/ | |__| |  __/ |  | | (_| (_) |
+ |_|  |_|\___/|_.__/ \__, | /_/    \_\___\___|  \____/ \___|_|  |_|\___\___/ 
+                      __/ |                                                  
+                     |___/ """
+print(first_name)
+
+# Hi Sir Val! The Ascii art generator I used is from your presentation in laboratory class: https://patorjk.com/software/taag/#p=display&f=Big&t=Hoby%20Ace%20Jerico
